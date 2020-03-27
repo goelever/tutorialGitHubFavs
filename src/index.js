fdee4e995ab9a250a774a63b45df272830a2a1ab
goelever <stefan.hartweg@web.de>
2020-03-27 20:41:00 +0100

    nach serviceWorker)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as serviceWorker from './serviceWorker';
 //import App from './components/App'; Warum geht das nicht mehr?
 //Importing modules using require, and exporting using module.exports and exports.foo.
 var App = require('./components/popular');
@@ -14,4 +15,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+serviceWorker.unregister();
