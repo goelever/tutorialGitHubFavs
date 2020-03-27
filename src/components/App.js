1eec7129821c85dbc7b913db770ef56e868adbc4
goelever <stefan.hartweg@web.de>
2020-03-27 10:44:10 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var Popular = require('./popular');
+
+// App.js // 
+class App extends React.Component {
+
+render() {
+return (
+<div className='container'>
+<Popular/>
+</div>
+
+   )
+  }
+}
+
+module.exports = App;
